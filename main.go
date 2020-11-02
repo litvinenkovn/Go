a8f049d1157b496d6bb9e5421697d473ea1e7474
Lytvynenko.v <litvinenkovn@ukr.net>
2020-11-02 23:06:11 +0200

    One day

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 // TEST..
-// TEST..
+// TEST.. // TEST.. // TEST.. // TEST../
 package main
 
 import (
